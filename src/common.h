8aae48a32248f7b27177849fc32189e7924342b8
Jaemin Lim <jmin81246ea@nave.com>
2023-11-01 13:32:05 +0900

    오류 수정

--- src/common.h
+++ src/common.h
@@ -4,6 +4,9 @@
 #include <memory>
 #include <string>
 #include <optional>
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+#include <spdlog/spdlog.h>
 
 std::optional<std::string> LoadTextFile(const std::string& filename);
 
